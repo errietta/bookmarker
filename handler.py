0301edd05f482d856d09471fc9cbc118b25316af
Errietta Kostala <errietta@errietta.me>
2018-07-31 20:45:52 +0100

    WIP :) handler code

--- handler.py
+++ handler.py
@@ -1,23 +1,38 @@
 import json
 from pprint import pprint
+from Bookmark.DB import createUser, createBookmark
+
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+  body = {
+    "message": "Go Serverless v1.0! Your function executed successfully!",
+    "input": event
+  }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+  response = {
+    "statusCode": 200,
+    "body": json.dumps(body)
+  }
 
-    return response
+  return response
+
 
 def addUrl(event, context):
-    pprint(event)
+  pprint(event)
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(event)
+  #createUser({
+  #    'userName': 'test',
+  #    'email': 'errietta@errietta.me',
+  #})
+
+  createBookmark(
+    'errietta@errietta.me',
+    {
+      'hello': 'world'
     }
+  )
+
+  return {
+    "statusCode": 200,
+    "body": json.dumps(event)
+  }
